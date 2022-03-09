62661a2dda6e70ce7ed453505a8aab971ef175c7
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2022-03-09 19:43:36 +0100

    poprawka szablonu, wylaczenie purge-css

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    plugins: {
-        '@fullhuman/postcss-purgecss': {
-            content: [
-                'layouts/**/*.html'
-            ],
-            whitelist: [
-            ]
+    /*
+        plugins: {
+            '@fullhuman/postcss-purgecss': {
+                content: [
+                    'layouts/!**!/!*.html'
+                ],
+                whitelist: [
+                    '#książki',
+                    'article'
+                ]
+            },
+            autoprefixer: {},
+            cssnano: { preset: 'default' }
         },
-        autoprefixer: {},
-        cssnano: { preset: 'default' }
-    },
+    */
     noMap: false
 }
